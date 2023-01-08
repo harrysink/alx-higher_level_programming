2fc6a67a7a072f7d7a8ca7ac8e131b02ead5ed88
harrysink <harrisonfemi@yahoo.com>
2023-01-08 12:51:46 +0000

    tasks update for 0x01.Python

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * *insert_node - this function inserts a number into a sorted singly linked
+ * list
+ *
+ */
+listint_t *insert_node(listint_t **head, int number);
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        if ord(x) >= ord('a') and ord(x) <= ord('z'):
+            char = chr(ord(x) - 32)
+        else:
+            char = x
+        print(f"{char:s}", end="")
+    print('')
