ddce67c172f7130ecf7d281b4fdd0f9e1d7107da
harrysink <harrisonfemi@yahoo.com>
2023-01-18 23:42:54 +0000

    task 10 fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best = []
     if not a_dictionary:
         return None
-    best = max(a_dictionary)
-    return best
+    return max(a_dictionary)
