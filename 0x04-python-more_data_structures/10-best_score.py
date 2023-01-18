08774b2e636a367991f38773c7eb4f5208da23d7
harrysink <harrisonfemi@yahoo.com>
2023-01-18 23:35:02 +0000

    task 10 hot fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,5 @@ def best_score(a_dictionary):
     best = []
     if not a_dictionary:
         return None
-    else:
-        best = max(a_dictionary)
+    best = max(a_dictionary)
     return best
