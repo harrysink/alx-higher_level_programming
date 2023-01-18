c591722764f4e96174da7ad797068b0a25c70f70
harrysink <harrisonfemi@yahoo.com>
2023-01-18 23:21:15 +0000

    task update for python more data structires project

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_string = []
+    if (roman_string == 'I'):
+    return 1
+    if (roman_string == 'V'):
+    return 5
+    if (roman_string == 'X'):
+    return 10
+    if (roman_string == 'L'):
+    return 50
+    if (roman_string == 'C'):
+    return 100
+    if (roman_string == 'D'):
+    return 500
+    if (roman_string == 'M'):
+    return 1000
