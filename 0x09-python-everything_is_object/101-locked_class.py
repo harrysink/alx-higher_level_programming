1eb45fbed326671c3821f5f690dff7b47adacec7
harrysink <harrisonfemi@yahoo.com>
2023-02-14 07:24:39 +0000

    tasks 30 & 34 hot fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-"""Module 101-locked_class
-Defines a LockedClass class.
-"""
-
-
 class LockedClass:
 
     __slots__ = ['first_name']
-    """Prevents the declaration of any variable
-    that isn't in __slots__
-    """
 
     def __init__(self, first_name=''):
-        """Initializes a LockedClass instance
-
-        Args:
-            first_name: first_name of individual
-        """
         self.first_name = first_name
